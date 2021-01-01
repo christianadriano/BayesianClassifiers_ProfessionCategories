af92b84fc416291d7bb694cbca53dcde9e08a1ba
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-01-01 23:38:10 +0100

    Create BayesLogistic_Profession_E2.R

---
+++ BayesLogistic_Profession_E2.R
@@ -0,0 +1,15 @@
+"
+Bayesian Logistic Regression to predict profession of participant in E2.
+
+This model will be later used to infer the profession of participants in experiment 1.
+Inferring professions in E1 will be later important to evaluate how well the causal models 
+generalize across the two experiments E1 and E2.
+
+We will compare different models based on different predictors:
+gender
+age
+years of programming
+qualification score
+adjusted qualification score
+
+"

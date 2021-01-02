e9208ad955535fffae6b377ef13657a1633b48b8
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-01-03 00:00:47 +0100

    Update BayesLogistic_Profession_E2.R

--- BayesLogistic_Profession_E2.R
+++ BayesLogistic_Profession_E2.R
@@ -13,8 +13,28 @@ qualification score
 adjusted qualification score
 
 "
-
+library(rethinking)
 library(dplyr)
 
 #Load consent data from E2
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E2.R")
+
+#create column with student and non-student
+df_consent$is_student = FALSE
+
+df_consent$profession_str <- as.character(df_consent$profession)
+df_consent[df_consent$profession_str=="Undergraduate_Student",]$is_student <- TRUE
+df_consent[df_consent$profession_str=="Graduate_Student",]$is_student <- TRUE
+
+#Approximate 
+
+# approximate posterior
+m13.1 <- ulam(
+  alist(
+    S ~ dbinom( N , p ) ,
+    logit(p) <- a[tank] ,
+    a[tank] ~ dnorm( a_bar , sigma ) ,
+    a_bar ~ dnorm( 0 , 1.5 ) ,
+    sigma ~ dexp( 1 )
+  ), 
+  data=dat , chains=4 , log_lik=TRUE )

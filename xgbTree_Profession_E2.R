340d4f3962962bd05e87e9fa1c9764f2a1dd17e5
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-01-08 22:37:13 +0100

    Create xgbTree_Profession_E2.R

---
+++ xgbTree_Profession_E2.R
@@ -0,0 +1,10 @@
+"
+XGBoostTree to predict professions (Student and Non-Student) for E2 participants
+
+Uses the Extreme Gradient Boosting, which is an efficient implementation 
+of the gradient boosting frame-work from Chen & Guestrin (2016) <doi:10.1145/2939672.2939785>
+
+"
+
+install.packages("xgboost")
+library(xgboost)

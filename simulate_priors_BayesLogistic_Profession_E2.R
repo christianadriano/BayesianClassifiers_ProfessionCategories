691d7d805b044cc04f64cb7adf56b7397c1bdd20
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-01-10 23:44:37 +0100

    Create simulate_priors_BayesLogistic_Profession_E2.R

---
+++ simulate_priors_BayesLogistic_Profession_E2.R
@@ -0,0 +1,25 @@
+"
+Simulate Priors for Logistic regression - BayesLogistic_Profession_E2.R
+
+Priors for the following coefficients:
+age
+years_programming
+qualification 
+
+"
+
+#------------------
+#Age
+
+
+#-----------------
+#Years Programming
+
+
+#-----------------
+#Qualification score
+
+
+
+#-----------------
+#Adjusted qualification

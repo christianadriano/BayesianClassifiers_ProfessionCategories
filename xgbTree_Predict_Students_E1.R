892123cb25fa1a5921e47e7bd3b33d13d3a828ed
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2021-07-14 00:29:25 +0200

    Update xgbTree_Predict_Students_E1.R

--- xgbTree_Predict_Students_E1.R
+++ xgbTree_Predict_Students_E1.R
@@ -71,16 +71,20 @@ runXGB_CrossValidation <- function(train.features,train.label){
                max_depth = 4, #standard parameterization is 6, we reduced to 4 to reduce overfitting
                eta = 0.3, #standard, the smaller, the less overfitting risk
                objective = "binary:logistic",
-               verbose=FALSE
+               verbose=FALSE,
+               prediction=TRUE
   );
   
   print(paste0("train_error= ",round(cv$evaluation_log$train_error_mean[cv$best_iteration],digits=4)));
   print(paste0("test_error= ",round(cv$evaluation_log$test_error_mean[cv$best_iteration],digits=4)));
-  
+ 
+  return(cv); 
 }
 
+#https://stackoverflow.com/questions/35050846/xgboost-in-r-how-does-xgb-cv-pass-the-optimal-parameters-into-xgb-train
+
 #-------------------------------------------
-runXGB_CrossValidation(
+model <- runXGB_CrossValidation(
   train.features = df_consent %>% select(age,years_programming),
   train.label = train.label 
 )
@@ -92,109 +96,10 @@ runXGB_CrossValidation(
 source("C://Users//Christian//Documents//GitHub//CausalModel_FaultUnderstanding//data_loaders//load_consent_create_indexes_E1.R")
 
 #create column with student and non-student
-df_consent$is_student = 0
+df1_consent$is_student = 0
 
-train.features <- df_consent %>% select(years_programming,age,qualification_score)
-train.label <- df_consent %>% select(is_student)
+train.features <- df1_consent %>% select(years_programming,age,qualification_score)
+train.label <- df1_consent %>% select(is_student)
 
 #-----------------------------
-
-runXGBoost <- function(train.features,train.label,rounds,depth){
-  bst <- xgboost(data = as.matrix(train.features), label = as.matrix(train.label),
-                 max_depth =depth,eta = 1, nthread = 4, 
-                 nrounds = rounds,eval_metric = list("error"), 
-                 objective = "binary:logistic",verbose = 1);
-  
-  return (bst$evaluation_log[rounds-1][[2]])
-}
-
-runXGB_CrossValidation <- function(train.features,train.label){
-  dtrain <- xgb.DMatrix(data = as.matrix(train.features), 
-                        label = as.matrix(train.label));
-  cv <- xgb.cv(data = dtrain, 
-               nrounds = 1000, #larger rounds did not give better results
-               early_stopping_rounds=500, #stop after error did not change and pick the best iteration
-               nfold = 10, #best practice, smaller value, 5 fold gave similar results
-               metrics = list("error"), #standard for binary classification
-               max_depth = 4, #standard parameterization is 6, we reduced to 4 to reduce overfitting
-               eta = 0.3, #standard, the smaller, the less overfitting risk
-               objective = "binary:logistic",
-               verbose=FALSE
-               );
-  
-  print(paste0("train_error= ",round(cv$evaluation_log$train_error_mean[cv$best_iteration],digits=4)));
-  print(paste0("test_error= ",round(cv$evaluation_log$test_error_mean[cv$best_iteration],digits=4)));
-  
-}
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age),
-  train.label = train.label 
-)
-
-#[1] "train_error= 0.2786"
-#[1] "test_error= 0.283"
-# Only Undergrads as students
-#[1] "train_error= 0.1934"
-#[1] "test_error= 0.1934"
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age,years_programming),
-  train.label = train.label 
-)
-
-#[1] "train_error= 0.2547"
-#[1] "test_error= 0.2768"
-# Only Undergrads as students
-#[1] "train_error= 0.188"
-#[1] "test_error= 0.2034"
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age,years_programming,qualification_score),
-  train.label = train.label
-  )
-
-#[1] "train_error= 0.2498"
-#[1] "test_error= 0.2791"
-# Only Undergrads as students
-#[1] "train_error= 0.1884"
-#[1] "test_error= 0.2014"
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age,years_programming,qualification_score,test_duration),
-  train.label = train.label
-)
-
-#[1] "train_error= 0.2245"
-#[1] "test_error= 0.2645"
-# Only Undergrads as students
-#[1] "train_error= 0.1705"
-#[1] "test_error= 0.1921"
-
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age,years_programming,adjusted_score,test_duration),
-  train.label = train.label 
-  )
-
-#[1] "train_error= 0.2152"
-#[1] "test_error= 0.2629"
-# Only Undergrads as students
-
-
-#-------------------------------------------
-runXGB_CrossValidation(
-  train.features = df_consent %>% select(age,years_programming,adjusted_score,test_duration,testDuration_fastMembership),
-  train.label = train.label 
-)
-
-#[1] "train_error= 0.1836"
-#[1] "test_error= 0.226"
-# Only Undergrads as students
-
 
